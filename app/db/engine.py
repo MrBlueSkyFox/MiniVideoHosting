4b21385e01052ad82d1882b08913016ae0cb1865
Tigran <tigranium123@gmail.com>
2023-12-01 12:28:12 +0400

    add async engine and session maker

---
+++ app/db/engine.py
@@ -0,0 +1,21 @@
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+
+from app.core.config import settings
+
+# from sqlmodel.ext.asyncio.session import AsyncSession
+
+connect_args = {"check_same_thread": False}
+engine = create_async_engine(
+    str(settings.SQLALCHEMY_DATABASE_URI),
+    echo=False,
+    future=True,
+    poolclass=NullPool,
+)
+
+
+async def get_session() -> AsyncSession:
+    async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+    async with async_session() as session:
+        yield session
