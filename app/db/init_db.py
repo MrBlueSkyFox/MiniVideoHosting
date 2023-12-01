895d765e8262887c632e48f75f99484dab9cb58f
Tigran <tigranium123@gmail.com>
2023-12-01 12:27:52 +0400

    add placeholder for manual db init

---
+++ app/db/init_db.py
@@ -0,0 +1,11 @@
+
+from app.db import base  # noqa: F401
+
+
+def init_db(session) -> None:
+    # Tables should be created with Alembic migrations
+    # But if you don't want to use migrations, create
+    # the tables un-commenting the next line
+    # Base.metadata.create_all(bind=engine)
+
+    pass
