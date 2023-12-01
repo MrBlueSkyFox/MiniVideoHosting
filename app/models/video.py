a1aa727dc59c6ba6632961539bcade57b7170ca0
Tigran <tigranium123@gmail.com>
2023-12-01 12:29:10 +0400

    define sql model for video  entity

---
+++ app/models/video.py
@@ -0,0 +1,12 @@
+import uuid
+
+from sqlalchemy import Column, String, UUID
+
+from app.db.base_class import Base
+
+
+class Video(Base):
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    title = Column(String)
+    description = Column(String)
+    filename = Column(String)
