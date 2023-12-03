be87dc46b6f004d1c6433180b90f370b107aac9b
Tigran <tigranium123@gmail.com>
2023-12-03 23:16:16 +0400

    move video api to separate file

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 # from api import video_router
 from starlette.templating import Jinja2Templates
 
-from app.api.api import video_router
+from app.api.api import api_router
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -10,10 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent
 app = FastAPI()
 templates = Jinja2Templates(directory=str(Path(BASE_DIR, 'templates')))
 
-app.include_router(video_router,
-                   prefix="/video",
-                   tags=["video"]
-                   )
+app.include_router(api_router)
 
 
 @app.get("/")
