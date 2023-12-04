d15f5e3b9f1fe96674fc3e39b7efca646493f777
Tigran <tigranium123@gmail.com>
2023-12-04 22:11:27 +0400

    path for var update

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from core.fast_api_users import auth_backend, fastapi_users_point
+from core.fast_api_users import auth_backend
+from api.deps import fastapi_users_point
 
 auth_router = APIRouter()
 auth_router.include_router(
