ca8cc7e49b88bf04f681442933235d8aa2c5440f
Tigran <tigranium123@gmail.com>
2023-12-04 20:58:00 +0400

    add new routing to main router

--- app/api/api.py
+++ app/api/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import video
+from app.api.endpoints import video, user, auth
 
 api_router = APIRouter()
 api_router.include_router(video.video_router, prefix="/video", tags=["video"])
+api_router.include_router(user.user_router, prefix="/user", tags=["user"])
+api_router.include_router(auth.auth_router, prefix="/auth", tags=["auth"])
