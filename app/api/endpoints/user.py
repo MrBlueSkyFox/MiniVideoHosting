25b7e27c8ee38bf17b567c557aec6d34aa3a9b6e
Tigran <tigranium123@gmail.com>
2023-12-04 22:11:14 +0400

    cleanup

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -1,16 +1,13 @@
 from fastapi import Depends, APIRouter
 
+from api.deps import current_active_user, fastapi_users_point
 from app.models.user import User
-from core.fast_api_users import fastapi_users_point
 from app.shemas.user import UserRead, UserCreate, UserUpdate
-
-current_active_user = fastapi_users_point.current_user(active=True)
 
 user_router = APIRouter()
 
 user_router.include_router(
     fastapi_users_point.get_register_router(UserRead, UserCreate),
-    # prefix="/",
     tags=["user"],
 )
 user_router.include_router(
@@ -18,11 +15,7 @@ user_router.include_router(
     prefix="/auth",
     tags=["user"],
 )
-# user_router.include_router(
-#     fastapi_users.get_verify_router(UserRead),
-#     prefix="/auth",
-#     tags=["auth"],
-# )
+
 user_router.include_router(
     fastapi_users_point.get_users_router(UserRead, UserUpdate),
     prefix="/user",
