2a900f8212d91fd4dff382e7b9daab85f0ca7952
Tigran <tigranium123@gmail.com>
2023-12-04 22:09:58 +0400

    update model for users

--- app/models/video.py
+++ app/models/video.py
@@ -1,12 +1,15 @@
 import uuid
 
-from sqlalchemy import Column, String, UUID
+from sqlalchemy import Column, String, UUID, ForeignKey
+from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
 
 
 class Video(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    owner_id = Column(UUID(as_uuid=True), ForeignKey('user.id'))
     title = Column(String)
     description = Column(String)
     filename = Column(String)
+    owner = relationship('User')
